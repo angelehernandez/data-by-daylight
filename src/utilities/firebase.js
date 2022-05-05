16cd28a9f9a2d1a90d6baba8904869034f894515
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2022-05-05 12:41:48 -0500

    add firebase config

---
+++ src/utilities/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { useState, useEffect } from 'react';
+import { getDatabase, onValue, ref, set, get } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBOr98EUaom7a0I6qVE_w0o2NVl05A7tOc",
+    authDomain: "data-by-daylight.firebaseapp.com",
+    databaseURL: "https://data-by-daylight-default-rtdb.firebaseio.com",
+    projectId: "data-by-daylight",
+    storageBucket: "data-by-daylight.appspot.com",
+    messagingSenderId: "634597468054",
+    appId: "1:634597468054:web:560187ad1986f2edf29fb7",
+    measurementId: "G-0VM9XV3YDD"
+  };
