4c281def29e112c9d4aed08cbf84264ad468581c
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2022-06-01 19:47:54 -0500

    add fake stacked bar chart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UploadButton from './components/Upload';
 import SignInButton from './components/SignIn';
+import StackedBarChart from './components/StackedBarChart';
+import data from './data/data';
+import colors from './data/colors';
+import keys from './data/keys';
+import "./styles/styles.css";
 import { useEffect, useState } from 'react';
 import { useData, useUserState } from './utilities/firebase';
 
@@ -15,12 +20,18 @@ function App() {
   return (
     <>
       <div className='App'>
+        
         <div className='Login'>
           <SignInButton/>
         </div>
+
         <div className='Content'>
           <Banner title={'Data by Daylight'} />
           <UploadButton/>
+        </div>
+
+        <div className='Data'>
+          <StackedBarChart colors={colors} data={data} keys={keys} />
         </div>
       </div>
     </>
